cae9e7d5828991f0c66589c2df0bb1651f7f79d3
Jesse Ren <jesseren80@gmail.com>
2021-04-26 12:05:25 -0400

    Update settings.py

--- valineups/settings.py
+++ valineups/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'l0y=_*143r59g+pt00ju=o3rbtd8quo&hrs3v6^dp#auu8=txv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
