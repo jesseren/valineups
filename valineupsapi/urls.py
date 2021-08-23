286a16ed2714eafd80325d02c107a745a2c38738
billwang80 <billwang80@users.noreply.github.com>
2021-08-22 22:40:24 -0400

    update api with filtering

--- valineupsapi/urls.py
+++ valineupsapi/urls.py
@@ -4,11 +4,12 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'lineups', views.LineupViewSet)
+# router.register(r'lineups', views.LineupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('lineups/', views.LineupView.as_view()),
 ]
--- valineupsapi/views.py
+++ valineupsapi/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework import generics
 
 from .serializers import LineupSerializer
 from .models import Lineup
 
 class LineupViewSet(viewsets.ModelViewSet):
-    queryset = Lineup.objects.all().order_by('agent')
+    # queryset = Lineup.objects.all().order_by('agent')
     serializer_class = LineupSerializer
+
+
+class LineupView(generics.ListAPIView):
+    serializer_class = LineupSerializer
+
+    def get_queryset(self):
+        queryset = Lineup.objects.all()
+        selected_agent = self.request.query_params.get('agent')
+        selected_map = self.request.query_params.get('map')
+        if selected_agent is not None:
+            queryset = queryset.filter(agent=selected_agent)
+        if selected_map is not None:
+            queryset = queryset.filter(gameMap=selected_map)
+        return queryset
