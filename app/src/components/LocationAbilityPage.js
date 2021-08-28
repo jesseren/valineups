e86c08dda9e2e2e983689476d31d248892ee0d7c
billwang80 <billwang80@users.noreply.github.com>
2021-08-28 15:33:56 -0400

    add abilites

--- app/src/components/LocationAbilityPage.js
+++ app/src/components/LocationAbilityPage.js
@@ -7,17 +7,19 @@ import store from "../store/store"
 const sites = ['a', 'b']
 
 const abilities = {
-    sova: ['Recon bolt', 'shock_dart', 'hunter_fury'],
-    viper: ['snakebite', 'poison_gas'],
+    'Sova': ['Recon bolt', 'shock_dart', 'hunter_fury'],
+    'Viper': ['snakebite', 'poison_gas'],
 }
 
 function LocationAbility(props) {
     const [ selectedSite, setSite ] = useState('')
     const [ selectedAbility, setAbility ] = useState('')
 
+    const selectedAgent = store.getState().agent
+
     // need logic for c site
     const siteSelection = sites.map((site) => 
-        <div className="valorantMap"
+        <div className="valorantMap" key={site}
             onClick={() => setSite(site)}
         >
             <h2>test</h2>
@@ -25,15 +27,30 @@ function LocationAbility(props) {
         </div>
     )
 
+    const abilitySelection = abilities[selectedAgent].map((ability) => 
+        <div className="valorantMap"
+            onClick={() => setAbility(ability)}
+        >
+            <h2>test</h2>
+            { ability[0].toUpperCase() + ability.slice(1) } 
+        </div>
+    )
 
     return (
         <div className="mapSelectionScreen">
             <Header />
             <img className='logo' src='images/Valorant_logo.png' />
+
             <h1>Select the site</h1> 
             <div className="mapSelection">
-                {siteSelection}
+                { siteSelection }
             </div>
+
+            <h1>Select the ability</h1> 
+            <div className="mapSelection">
+                { abilitySelection }
+            </div>
+
             <div className="nextButton">
                 <Link to="lineups">
                     <p>Submit</p>
