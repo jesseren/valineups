8b2b35b73b92ce85c1b5b77c5d336a7c67e63b11
billwang80 <billwang80@users.noreply.github.com>
2021-09-22 22:23:39 -0400

    fix click area

--- app/src/components/AbilityCard.js
+++ app/src/components/AbilityCard.js
@@ -4,13 +4,12 @@ import React from "react"
 function AbilityCard(props) {
     return (
         // <div className="ability_container">
-        <div className={`ability_container${ props.selectedAbility == props.name ? '_selected':'' }`}>
+        <div 
+            className={`ability_container${ props.selectedAbility == props.name ? '_selected':'' }`}
+            onClick={() => props.setAbility( props.name )}
+        >
             <h2>{props.name}</h2>
-            <span onClick={() => {
-                props.setAbility( props.name )
-            }}>
-                <img src={props.imgurl} alt={props.name}/>
-            </span>
+            <img src={props.imgurl} alt={props.name}/>
         </div>
     )
 }
