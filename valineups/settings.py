bab7fa36af6b7402dc0a045794a322fa017892e1
billwang80 <billwang80@users.noreply.github.com>
2022-01-04 15:35:07 -0500

    modify settings py

--- valineups/settings.py
+++ valineups/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'valineups.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'valineups_db',
+        'USER': 'valineups_admin',
+        'PASSWORD': 'evangelpp',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
